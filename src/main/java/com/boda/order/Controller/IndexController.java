de7a29182523331313ba7d5628bb22c045f719dd
Yang <94277424+YaaangZ@users.noreply.github.com>
2023-05-03 16:44:35 +0100

    first commit

---
+++ src/main/java/com/boda/order/Controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.boda.order.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/hello")
+    public String getIndexPage(Model model) {
+        model.addAttribute("message", "Hello, world!");
+        return "index";
+    }
+}
---
+++ src/main/java/com/boda/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.boda.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+
+    }
+}
